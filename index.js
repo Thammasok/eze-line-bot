c9dd2580cbdf592404f6d8d67978b3c0323c6510
Jaranchai <jaranchai.nt@hotmail.com>
2018-04-14 21:58:04 +0700

    add link webhook

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 5000))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
 	res.send('Hello')
-})
+});
+
+app.post('/callback', (req, res) => {
+    res.sendStatus(200)
+});
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
-})
+});
