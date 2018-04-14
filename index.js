2242eaff5413818f546a019e5e5846a7d430e45d
Jaranchai <jaranchai.nt@hotmail.com>
2018-04-14 22:19:26 +0700

    test push msg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
-
+	
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -10,14 +10,51 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+const channelAccessToken = '5YyI0WllyPvKou0xttX8W0qacW3C0i96J/+97kxA6Xhxjpu7i/QDeanvfUYZfujOtsbGwuJWSf5TIe4YXnAKJTSRkzxmj9RWAxMLhF9TT89Qg0nPgqFu9eIPEZ33F5iU0+Cu2gWWO4j7ZzzwnAzfvAdB04t89/1O/w1cDnyilFU=';
+const channelSecret = 'b2c3273b876d42277eb18dcba0dfe9aa';
 
 app.get('/', function (req, res) {
 	res.send('Hello')
 });
 
 app.post('/callback', (req, res) => {
-    res.sendStatus(200)
+	var text = req.body.events[0].message.text
+	var sender = req.body.events[0].source.userId
+	var replyToken = req.body.events[0].replyToken
+	console.log(text, sender, replyToken)
+	console.log(typeof sender, typeof text)
+	// console.log(req.body.events[0])
+	if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+		sendText(sender, text)
+	}
+	res.sendStatus(200)
 });
+
+function sendText (sender, text) {
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยของคุณ'
+      }
+    ]
+  }
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${channelAccessToken}'
+    },
+    url: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  })
+}
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
