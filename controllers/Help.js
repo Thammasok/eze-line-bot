eba66153c19e7d74f6c90d5e25cd905d59ae3a0d
Jaranchai <jaranchai.nt@hotmail.com>
2018-04-16 08:57:14 +0700

    import line bot

--- controllers/Help.js
+++ controllers/Help.js
@@ -1,3 +1,9 @@
+const line = require('@line/bot-sdk');
+
+const client = new line.Client({
+	channelAccessToken: '5YyI0WllyPvKou0xttX8W0qacW3C0i96J/+97kxA6Xhxjpu7i/QDeanvfUYZfujOtsbGwuJWSf5TIe4YXnAKJTSRkzxmj9RWAxMLhF9TT89Qg0nPgqFu9eIPEZ33F5iU0+Cu2gWWO4j7ZzzwnAzfvAdB04t89/1O/w1cDnyilFU='
+});
+
 exports.help = function (sender) {
 	return new Promise(() => {
 	const message = {
