6cd2dfaf42192b0aba780550a1e0d6d132c3032d
Jaranchai <jaranchai.nt@hotmail.com>
2018-04-16 21:25:51 +0700

    fix bug

--- controllers/Leave.js
+++ controllers/Leave.js
@@ -5,16 +5,26 @@ const client = new line.Client({
 	channelAccessToken: config.line.CHANNEL_ACCESS_TOKEN
 });
 
-exports.leave = function (sender, type) {
+exports.leave = function (sender, leaveType) {
   return new Promise(() => {
     /**
      * Create Leave :: DB
      * Field: id, userId(Sender), leave_type, status, [leave_from, leave_to], created_at, updated_at 
      */
 
+     let leave_type = 2;
+    //  if(leaveType === 'sick') {
+    //    leave_type = 1;
+    //  } else if (leaveType === 'annual') {
+    //    leave_type = 3;
+    //  } else {
+    //   //personal business
+    //   leave_type = 2;
+    //  }
+
     const message = {
       "type": "template",
-      "altText": "this is a confirm template",
+      "altText": "Leave date",
       "template": {
         "type": "leave date",
         "text": "ต้องการลาในวันที่",
@@ -39,7 +49,7 @@ exports.leave = function (sender, type) {
 			})
 			.catch((err) => {
 				// error handling
-				console.log(err.message);
+				console.log(err);
 			});
   });
 };
