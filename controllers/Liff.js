08648512027233a7dc46cf49bcb0f701e197f512
Jaranchai <jaranchai.nt@hotmail.com>
2018-08-05 17:16:47 +0700

    change pkm

--- controllers/Liff.js
+++ controllers/Liff.js
@@ -1,36 +1,38 @@
-require('whatwg-fetch')
+import axios from 'axios';
 
-exports.openLiff = function(sender) {
-  fetch('https://api.line.me/liff/v1/apps', {
-    method: "POST",
-    body: {
-      "view": {
-        "type":"tall",
-        "url":"https://eze-ex-leff.herokuapp.com"
-      }
-    },
+export function openLiff(sender) {
+  axios({
+    method: 'post',
+    url: 'https://api.line.me/liff/v1/apps',
     headers: {
       "Authorization": "Bearer " + process.env.CHANNEL_ACCESS_TOKEN,
       "Content-Type": "application/json"
     },
-    credentials: "same-origin"
-  }).then(function(response) {
-    if(response.status === 200) {
-      const message = {
-        "type": "text",
-        "text": "line://app/" + response.liffId
-      };
-    
-      client.pushMessage(sender, message)
-        .then(() => {
-          console.log('success');
-        })
-        .catch((err) => {
-          // error handling
-          console.log(err);
-        });
+    data: {
+      "view": {
+        "type":"tall",
+        "url":"https://eze-ex-leff.herokuapp.com"
+      }
     }
-  }, function(error) {
-    console.log(error.message)
+  }).then(function (response) {
+    console.log(response)
+    // if(response.status === 200) {
+    //   const message = {
+    //     "type": "text",
+    //     "text": "line://app/" + response.liffId
+    //   };
+    
+    //   client.pushMessage(sender, message)
+    //     .then(() => {
+    //       console.log('success');
+    //     })
+    //     .catch((err) => {
+    //       // error handling
+    //       console.log(err);
+    //     });
+    // }
+  })
+  .catch(function (error) {
+    console.log(error);
   })
 }
