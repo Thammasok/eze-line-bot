08237d011dd23aeecf0387c08be1b30e994a7ec7
Jaranchai <jaranchai.nt@hotmail.com>
2018-08-06 10:12:54 +0700

    remove code

--- controllers/Liff.js
+++ controllers/Liff.js
@@ -23,103 +23,22 @@ exports.openLiff = function(sender) {
     if(response.status === 200) {
 
       // Show Type Text Massage
-      // const message = {
-      //   "type": "text",
-      //   "text": liffUrl
-      // };
+      const message = {
+        "type": "text",
+        "text": "line://app/"+ response.data.liffId
+      };
 
-      // client.pushMessage(sender, message)
-      //   .then(() => {
-      //     console.log('success');
-      //   })
-      //   .catch((err) => {
-      //     // error handling
-      //     console.log(err);
-      //   });
-
-      axios({
-        method: 'post',
-        url: 'https://api.line.me/v2/bot/message/push',
-        headers: {
-          "Authorization": "Bearer " + process.env.CHANNEL_ACCESS_TOKEN,
-          "Content-Type": "application/json"
-        },
-        data: {
-          "to": sender,
-          "messages": [{
-            "type": "carousel",
-            "contents": [
-              {
-                "type": "bubble",
-                "body": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "First bubble"
-                    }
-                  ]
-                }
-              },
-              {
-                "type": "bubble",
-                "body": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "Second bubble"
-                    }
-                  ]
-                }
-              }
-            ]
-          }]
-          // "messages": [{
-          //   "type": "carousel",
-          //   "contents": [
-          //     {
-          //       "type": "bubble",
-          //       "body": {
-          //         "type": "box",
-          //         "layout": "horizontal",
-          //         "contents": [
-          //           {
-          //             "type": "text",
-          //             "text": "Click Open button"
-          //           }
-          //         ]
-          //       },
-          //       "footer": {
-          //         "type": "box",
-          //         "layout": "horizontal",
-          //         "contents": [
-          //           {
-          //             "type": "button",
-          //             "style": "primary",
-          //             "color": "#5DADE2",
-          //             "action": {
-          //               "type": "uri",
-          //               "label": "Open",
-          //               "uri": "line://app/"+ response.data.liffId
-          //             }
-          //           }
-          //         ]
-          //       }
-          //     }
-          //   ]
-          // }]
-        }
-      }).then(function (response) {
-        console.log('success')
-      }).catch(function (error) {
-        console.log(error);
+      client.pushMessage(sender, message)
+      .then(() => {
+        console.log('success');
       })
+      .catch((err) => {
+        // error handling
+        console.log(err);
+      });
     }
   })
   .catch(function (error) {
-    console.log(error.data.details);
+    console.log(error.);
   })
 }
